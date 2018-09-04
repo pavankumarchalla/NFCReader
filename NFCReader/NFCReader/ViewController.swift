8f9a8bb34930e58a88ffd9caf106720380fbf41b
Pavan Kumar C <pavan.challa23@gmail.com>
2018-09-04 17:44:16 +0530

    NFCReader single view application created

---
+++ NFCReader/NFCReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NFCReader
+//
+//  Created by Pavan Kumar C on 04/09/18.
+//  Copyright © 2018 pavan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
