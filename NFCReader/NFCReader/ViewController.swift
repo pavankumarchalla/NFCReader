51005c248aa1cedce2a302e4e7f1b9a407d34347
Pavan Kumar C <pavan.challa23@gmail.com>
2018-09-04 18:34:44 +0530

    CoreNFC implemented

--- NFCReader/NFCReader/ViewController.swift
+++ NFCReader/NFCReader/ViewController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+
+    var nfcSession: NFCNDEFReaderSession?
 
     @IBOutlet weak var statusLabel: UILabel!
+
+    //MARK: - Controller life cycle methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +25,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
+    //MARK: - IBAction Delegate methods
+
     @IBAction func scanClicked(_ sender: UIButton) {
-        
+        nfcSession = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+        nfcSession?.begin()
+    }
+
+    //MARK:- NFC Reader Session delegate methods
+
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("The session was invalidated: \(error.localizedDescription)")
+    }
+
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        // Parse the card's information
+        var result = ""
+        for payload in messages[0].records {
+            result += String.init(data: payload.payload, encoding: .utf8)! // 1
+        }
+
+        DispatchQueue.main.async {
+            self.statusLabel.text = result
+        }
     }
 
 }
